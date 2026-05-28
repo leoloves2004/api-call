2d2fac6a57b1419059342abe5ff46f2f86db0f14
leoloves2004 <leomhelanthwal11@gmail.com>
2026-05-28 19:39:20 +0800

    Student API CRUD project

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/students', 
+[StudentController::class, 'index']);
+
+Route::get('/students/{id}', 
+[StudentController::class, 'show']);
+
+Route::post('/students', 
+[StudentController::class, 'store']);
+
+Route::put('/students/{id}', 
+[StudentController::class, 'update']);
+
+Route::patch('/students/{id}', 
+[StudentController::class, 'patch']);
+
+Route::delete('/students/{id}',
+ [StudentController::class, 'destroy']);
+
+Route::delete('/students', 
+[StudentController::class, 'destroyAll']);
